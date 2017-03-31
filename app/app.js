7285fd7318fa953732272a78ad7ddb05246ed927
Casey Dailey <casey@feasttogether.org>
2017-03-30 23:39:57 -0500

    styled throughout and tinkered with layout etc

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('app', ['ngRoute', 'ngToast']);
+var app = angular.module('app', ['ngRoute', 'ngToast', 'angular-loading-bar']);
 
 
 console.log("app is in");
